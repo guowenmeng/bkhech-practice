22ee242209d24b85e886750f0225696620e9402d
guowm <guowm@5fan.com>
2019-07-18 14:01:38 +0800

    feat: 随机数测试

--- src/main/java/com/bkhech/home/practice/utils/RandomStringUtil.java
+++ src/main/java/com/bkhech/home/practice/utils/RandomStringUtil.java
@@ -20,4 +20,30 @@ public class RandomStringUtil {
         }
         return new String(c);
     }
+
+    /**
+     * Random random = new Random();//默认构造方法
+     * Random random = new Random(1000);//指定种子数字
+     *
+     * 在进行随机时，随机算法的起源数字称为种子数(seed)，在种子数的基础上进行一定的变换，从而产生需要的随机数字。
+     * 相同种子数的Random对象，相同次数生成的随机数字是完全相同的。也就是说，两个种子数相同的Random对象，第一次
+     * 生成的随机数字完全相同，第二次生成的随机数字也完全相同。
+     * 事实上，Random是一种伪随机数，相同的种子产生相同的序列
+     */
+    public static void random() {
+        System.out.println("Math.random() = " + Math.random());
+        ThreadLocalRandom current = ThreadLocalRandom.current();
+        System.out.println("ThreadLocalRandom.current() = " + current.nextInt(0,10));
+
+        Random r2 = new Random(50);//种子为50的对象
+        Random r3 = new Random(50);//种子为50的对象
+        //如果两个Random对象种子数相同，那么他们生成的结果将是一样。可以使用当前时间作为种子：System.currentTimeMillis()
+        System.out.println("r2.nextInt():"+r2.nextInt(10)+"-------r3.nextInt():"+r3.nextInt(10));
+    }
+
+    public static void main(String[] args) {
+        random();
+    }
+
+
 }
