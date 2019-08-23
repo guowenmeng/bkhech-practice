7a47e13ac26369efa1417a0f831309376a6eee1a
郭文猛 <guowm@5fun.com>
2019-08-23 12:26:06 +0800

    feat: deque
    使用Collections.asLifoQueue 单做LIFO使用，替代 Stack

---
+++ src/main/java/com/bkhech/home/practice/javacore/collection/deque/DequeAsStack.java
@@ -0,0 +1,34 @@
+package com.bkhech.home.practice.javacore.collection.deque;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+/**
+ * @author guowm[guowm@5fun.com]
+ * @date 2019/8/23
+ *
+ * 官方建议使用Deque替代Stack: Line 134
+ * Deques can also be used as LIFO (Last-In-First-Out) stacks.  This
+ * interface should be used in preference to the legacy {@link Stack} class.
+ */
+public class DequeAsStack {
+
+    public static void stack() {
+        Stack<Integer> stack = new Stack<>();
+        IntStream.range(0,10).forEach(item -> stack.add(item));
+        Integer stackElement = stack.pop();
+        System.out.println("stackElement = " + stackElement);
+    }
+
+    public static void dequeAsStack() {
+        Queue asLifoQueue = Collections.asLifoQueue(new ArrayDeque());
+        IntStream.range(0,10).forEach(item -> asLifoQueue.add(item));
+        Object poll = asLifoQueue.poll();
+        System.out.println("poll = " + poll);
+    }
+
+    public static void main(String[] args) {
+        dequeAsStack();
+    }
+
+}
