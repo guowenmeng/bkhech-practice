d6cb293889f058380708c371cc0129bbb1ad2a30
郭文猛 <guowm@5fun.com>
2019-08-23 11:49:29 +0800

    feat: 运算符

---
+++ src/main/java/com/bkhech/home/practice/javacore/core/运算符/Operator.java
@@ -0,0 +1,24 @@
+package com.bkhech.home.practice.javacore.core.运算符;
+
+/**
+ * @author guowm[guowm@5fun.com]
+ * @date 2019/8/20
+ */
+public class Operator {
+
+    /**
+     * ~（取反运算符）
+     */
+    private static void test() {
+        int i = ~37;
+        int j = ~-4;
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+    }
+
+    public static void main(String[] args) {
+        test();
+    }
+
+
+}
