bb2eeda54cd33a1bc26d1af0e5c22ec269b20938
郭文猛 <guowm@5fun.com>
2020-07-08 10:21:41 +0800

    fix: tokenizer: 结巴分词

--- src/main/java/com/bkhech/home/practice/tokenizer/jieba/JiebaTest.java
+++ src/main/java/com/bkhech/home/practice/tokenizer/jieba/JiebaTest.java
@@ -24,8 +24,8 @@ public class JiebaTest {
     public void testDemo() {
         JiebaSegmenter segmenter = new JiebaSegmenter();
         String[] sentences =
-                new String[] {"这是一个伸手不见五指的黑夜。我叫孙悟空，我爱北京，我爱Python和C++。", "我不喜欢日本和服。", "雷猴回归人间。",
-                        "工信处女干事每月经过下属科室都要亲口交代24口交换机等技术性器件的安装工作", "结果婚的和尚未结过婚的"};
+                new String[] {"这是一个伸手不见五指的黑夜。我叫孙悟空，我爱北京，我爱Python和C++。",
+                        "我不喜欢日本和服。", "雷猴回归人间。", "我觉得Jieba还不错！我是王婆！"};
         for (String sentence : sentences) {
             System.out.println(segmenter.process(sentence, JiebaSegmenter.SegMode.INDEX).toString());
         }
@@ -36,7 +36,7 @@ public class JiebaTest {
      */
     public void tfidf() {
 //        String content="孩子上了幼儿园 安全防拐教育要做好";
-        String content="这是一个伸手不见五指的黑夜。我叫孙悟空，我爱北京，我爱Python和C++。";
+        String content="我觉得Jieba还不错！我是王婆！";
         int topN=5;
         TFIDFAnalyzer tfidfAnalyzer=new TFIDFAnalyzer();
         List<Keyword> list=tfidfAnalyzer.analyze(content,topN);
