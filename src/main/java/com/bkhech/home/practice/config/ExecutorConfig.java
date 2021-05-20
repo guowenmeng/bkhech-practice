b35c4b022dd7935e6688bdee882b6ba654ae1da8
郭文猛 <guowm@5fun.com>
2021-05-20 11:32:03 +0800

    feat: MySQL: add Mysql-architecture.png

--- src/main/java/com/bkhech/home/practice/config/ExecutorConfig.java
+++ src/main/java/com/bkhech/home/practice/config/ExecutorConfig.java
@@ -1,14 +1,10 @@
 package com.bkhech.home.practice.config;
 
-import com.alibaba.fastjson.JSONObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class ExecutorConfig {
