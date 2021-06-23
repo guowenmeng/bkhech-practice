3718bee155ce896b6612ef88f3170657e4eeaa0b
郭文猛 <guowm@5fun.com>
2021-06-23 11:14:23 +0800

    feat: leetcode: 1 两数之和

---
+++ src/main/java/com/bkhech/home/practice/leetcode/editor/cn/[1]两数之和.java
@@ -0,0 +1,89 @@
+//给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那 两个 整数，并返回它们的数组下标。 
+//
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。 
+//
+// 你可以按任意顺序返回答案。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [2,7,11,15], target = 9
+//输出：[0,1]
+//解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [3,2,4], target = 6
+//输出：[1,2]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [3,3], target = 6
+//输出：[0,1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 2 <= nums.length <= 104 
+// -109 <= nums[i] <= 109 
+// -109 <= target <= 109 
+// 只会存在一个有效答案 
+// 
+//
+// 进阶：你可以想出一个时间复杂度小于 O(n2) 的算法吗？ 
+// Related Topics 数组 哈希表 
+// 👍 11397 👎 0
+
+package com.bkhech.home.practice.leetcode.editor.cn;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 两数之和
+ * guowm
+ * 2021-06-23 10:01:04
+ */
+class TwoSum{
+    public static void main(String[] args) {
+        Solution solution = new TwoSum().new Solution();
+        int[] nums = new int[]{7,2,11,15};
+        int target = 9;
+        final int[] twoSum = solution.twoSum(nums, target);
+        System.out.println(Arrays.toString(twoSum));
+    }
+ 
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length < 2) {
+            return null;
+        }
+
+        Map<Integer, Integer> numsMap = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            int key = target - nums[i];
+            if (numsMap.containsKey(key)) {
+                // numsMap.get(key) 的下标永远小于 当前 i 的下标值
+                return new int[]{numsMap.get(key), i};
+            } else {
+                numsMap.putIfAbsent(nums[i], i);
+            }
+        }
+
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
