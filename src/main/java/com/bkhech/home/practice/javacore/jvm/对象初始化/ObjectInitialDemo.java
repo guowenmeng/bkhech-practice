96ea8c306dadce8468c541fdadd964a978be356f
郭文猛 <guowm@5fun.com>
2021-09-16 19:59:45 +0800

    feat: java core:对象加载，初始化过程

---
+++ src/main/java/com/bkhech/home/practice/javacore/jvm/对象初始化/ObjectInitialDemo.java
@@ -0,0 +1,12 @@
+package com.bkhech.home.practice.javacore.jvm.对象初始化;
+
+/**
+ * 对象加载，初始化过程
+ * @author guowm
+ * @date 2021/9/16
+ */
+public class ObjectInitialDemo {
+    public static void main(String[] args) {
+        final User user = new User("1", "1", "1");
+    }
+}
