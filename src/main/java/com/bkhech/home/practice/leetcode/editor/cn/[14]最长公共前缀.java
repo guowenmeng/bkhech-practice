50eb88249ff8d46a9ea9ca11f7ef5af0bcfbf3e2
郭文猛 <guowm@5fun.com>
2021-10-12 19:58:35 +0800

    refactor: leetcode 14: 最长公共前缀 1.新增横向扫描法； 2.新增分治法

--- src/main/java/com/bkhech/home/practice/leetcode/editor/cn/[14]最长公共前缀.java
+++ src/main/java/com/bkhech/home/practice/leetcode/editor/cn/[14]最长公共前缀.java
@@ -43,10 +43,100 @@ class LongestCommonPrefix{
          final String ans = new LongestCommonPrefix().new Solution().longestCommonPrefix(strs);
          System.out.println(ans);
      }
- 
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+        // 方法三：分治法
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        return longestCommonPrefix(strs, 0, strs.length - 1);
+    }
+
+    private String longestCommonPrefix(String[] strs, int start, int end) {
+        if (start == end) {
+            return strs[start];
+        } else {
+            int mid = start + (end - start) / 2;
+            String lcpLeft = longestCommonPrefix(strs, start, mid);
+            String lcpRight = longestCommonPrefix(strs, mid + 1, end);
+            return commonPrefix(lcpLeft, lcpRight);
+        }
+    }
+
+    private String commonPrefix(String lcpLeft, String lcpRight) {
+        int minLength = Math.min(lcpLeft.length(), lcpRight.length());
+        for (int i = 0; i < minLength; i++) {
+            if (lcpLeft.charAt(i) != lcpRight.charAt(i)) {
+                return lcpLeft.substring(0, i);
+            }
+        }
+        return lcpLeft.substring(0, minLength);
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    /**
+     * 方法二：纵向扫描
+     * @param strs
+     * @return
+     */
+    public String longestCommonPrefixV3(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        int length = strs[0].length();
+        int count = strs.length;
+        for (int i = 0; i < length; i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 0; j < count; j++) {
+                //i == strs[j].length() 条件成立时：表示数组中存在一个元素的最大字符长度等于当前公共子串（在 i 位置处）
+                //strs[j].charAt(i) != c 条件成立时：表示数组中存在一个元素在索引 j 处的字符不相等
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+
+    /**
+     * 方法一：横向扫描
+     * @param strs
+     * @return
+     */
+    public String longestCommonPrefixV2(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        int count = strs.length;
+        for (int i = 1; i < count; i++) {
+            prefix = longestCommonPrefix(prefix, strs[i]);
+            if (prefix.length() == 0) {
+                break;
+            }
+        }
+        return prefix;
+    }
+
+    private String longestCommonPrefix(String str1, String str2) {
+        int length = Math.min(str1.length(), str2.length());
+        int index = 0;
+        while (index < length && str1.charAt(index) == str2.charAt(index)) {
+            index ++;
+        }
+        return str1.substring(0, index);
+    }
+
+    /**
+     * my version. 本质上是方法二 纵向扫描
+     * @param strs
+     * @return
+     */
+    public String longestCommonPrefixV1(String[] strs) {
         if (strs == null || strs.length == 0) {
             return "";
         }
@@ -82,7 +172,4 @@ class Solution {
 
         return ans.toString();
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)
-
 }
