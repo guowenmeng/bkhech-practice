2a080ec3269d4041407fe6f3db3ab006e21d39b7
郭文猛 <guowm@5fun.com>
2021-10-14 20:01:23 +0800

    refactor: leetcode 33: 搜索旋转数组（待测试）

--- src/main/java/com/bkhech/home/practice/leetcode/editor/cn/[33]搜索旋转排序数组.java
+++ src/main/java/com/bkhech/home/practice/leetcode/editor/cn/[33]搜索旋转排序数组.java
@@ -49,21 +49,87 @@
 package com.bkhech.home.practice.leetcode.editor.cn;
 
 /**
- * TODO
+ * 搜索旋转排序数组
  * guowm
  * 2021-07-08 18:57:09
  */
 class SearchInRotatedSortedArray{
      public static void main(String[] args) {
-         // TODO test
+         final Solution solution = new SearchInRotatedSortedArray().new Solution();
+         int[] nums = new int[]{4,5,6,7,0,1,2};
+         int target = 0;
+         int index = solution.search(nums, target);
+         System.out.println(index);
      }
  
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    /**
+     * 二分查找法
+     *  对于有序数组，可以使用二分查找的方法查找元素。需要注意的是，二分的写法有很多种，
+     *  所以在判断 target 大小与有序部分关系的时候可能会出现细节上的差别
+     * Time: O(logn)
+     * Space: O(1)
+     * @param nums
+     * @param target
+     * @return
+     */
     public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            //左半部分有序，一定是<=
+            if (nums[left] <= nums[mid]) {
+                //target 值在左半边
+                if (nums[left] < target && target <= nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+                //右半部分有序
+            } else {
+                //target 值在右半边
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
         return -1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
+    /**
+     * 暴力搜索
+     * Time: O(n)
+     * Space: O(1)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int searchV1(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
