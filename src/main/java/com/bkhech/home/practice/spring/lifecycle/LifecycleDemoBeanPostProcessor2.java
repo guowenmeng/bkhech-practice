22e656afcaf1e35ea76d0b96ec77d2aed06813b8
郭文猛 <guowm@5fun.com>
2021-12-17 19:01:55 +0800

    refactor: LifecycleDemo：解析 spring bean 声明周期

---
+++ src/main/java/com/bkhech/home/practice/spring/lifecycle/LifecycleDemoBeanPostProcessor2.java
@@ -0,0 +1,40 @@
+package com.bkhech.home.practice.spring.lifecycle;
+
+import com.bkhech.home.practice.spring.springioc.configuration.User;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+/**
+ * LifecycleDemoBeanPostProcessor
+ * BeanPostProcessor：所有 bean 的后置处理器
+ * 所有的 bean 都已经实例化完成（see {@link LifecycleDemo}），
+ * 然后所有 bean 都会回调 BeanPostProcessor 里的两个方法，所以要根据条件处理业务逻辑
+ *
+ * @author guowm
+ * @date 2021/5/14
+ */
+public class LifecycleDemoBeanPostProcessor2 implements BeanPostProcessor {
+
+    public LifecycleDemoBeanPostProcessor2() {
+        System.out.println("LifecycleDemoBeanPostProcessor2 constructor");
+    }
+
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        // 根据条件处理业务逻辑
+        if (bean instanceof LifecycleDemo || bean instanceof LifecycleDemo2 || bean instanceof User) {
+            System.out.println("LifecycleDemoBeanPostProcessor2 postProcessBeforeInitialization----" + beanName);
+        }
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof LifecycleDemo || bean instanceof LifecycleDemo2 || bean instanceof User) {
+            System.out.println("LifecycleDemoBeanPostProcessor2 postProcessAfterInitialization----" + beanName);
+        }
+        return bean;
+    }
+
+}
