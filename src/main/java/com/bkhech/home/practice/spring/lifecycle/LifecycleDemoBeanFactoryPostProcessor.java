06016c91ed5bc2b53212d213c06cdada847f2177
郭文猛 <guowm@5fun.com>
2021-12-17 19:01:41 +0800

    refactor: LifecycleDemo：解析 spring bean 声明周期

---
+++ src/main/java/com/bkhech/home/practice/spring/lifecycle/LifecycleDemoBeanFactoryPostProcessor.java
@@ -0,0 +1,32 @@
+package com.bkhech.home.practice.spring.lifecycle;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+/**
+ * BeanFactoryPostProcessor
+ *  Spring的BeanFactoryPostProcessor和BeanPostProcessor:
+ * {@literal https://blog.csdn.net/caihaijiang/article/details/35552859}
+ *
+ * @author guowm
+ * @date 2021/12/17
+ */
+public class LifecycleDemoBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+    public LifecycleDemoBeanFactoryPostProcessor() {
+        System.out.println("LifecycleDemoBeanFactoryPostProcessor constructor");
+    }
+
+    /**
+     * Modify the application context's internal bean factory after its standard
+     * properties even to eager-initializing beans.
+     *
+     * @param beanFactory the bean factory used by the application context
+     * @throws BeansException in case of errors
+     */
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        System.out.println("LifecycleDemoBeanFactoryPostProcessor postProcessBeanFactory");
+    }
+}
--- src/main/java/com/bkhech/home/practice/spring/lifecycle/LifecycleDemoBeanPostProcessor.java
+++ src/main/java/com/bkhech/home/practice/spring/lifecycle/LifecycleDemoBeanPostProcessor.java
@@ -1,11 +1,16 @@
 package com.bkhech.home.practice.spring.lifecycle;
 
+import com.bkhech.home.practice.spring.springioc.configuration.User;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
 
 /**
  * LifecycleDemoBeanPostProcessor
+ * BeanPostProcessor：所有 bean 的后置处理器
+ * 所有的 bean 都已经实例化完成（see {@link LifecycleDemo}），
+ * 然后所有 bean 都会回调 BeanPostProcessor 里的两个方法，所以要根据条件处理业务逻辑
+ *
  * @author guowm
  * @date 2021/5/14
  */
@@ -18,16 +23,17 @@ public class LifecycleDemoBeanPostProcessor implements BeanPostProcessor {
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        if (bean instanceof LifecycleDemo) {
-            System.out.println("LifecycleDemo postProcessBeforeInitialization----" + beanName);
+        // 根据条件处理业务逻辑
+        if (bean instanceof LifecycleDemo || bean instanceof LifecycleDemo2 || bean instanceof User) {
+            System.out.println("LifecycleDemoBeanPostProcessor postProcessBeforeInitialization----" + beanName);
         }
         return bean;
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        if (bean instanceof LifecycleDemo) {
-            System.out.println("LifecycleDemo postProcessAfterInitialization----" + beanName);
+        if (bean instanceof LifecycleDemo || bean instanceof LifecycleDemo2 || bean instanceof User) {
+            System.out.println("LifecycleDemoBeanPostProcessor postProcessAfterInitialization----" + beanName);
         }
         return bean;
     }
